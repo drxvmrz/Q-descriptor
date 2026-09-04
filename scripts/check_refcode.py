f4fcafff1712748493bc122de2534ab12f17972e
Paul D. <pddrozhilkin@yandex.ru>
2026-09-04 12:03:54 +0300

    new

---
+++ scripts/check_refcode.py
@@ -0,0 +1,20 @@
+###############################################################################
+#
+# This script checks if a REFCODEs in database
+#
+# INITS ########################################################################
+
+__version__ = "1.0.0"
+__author__ = "Pavel D. Drozhilkin"
+__email__ = "pddrozhilkin@yandex.ru"
+
+# IMPORTS ######################################################################
+
+import os
+import sys
+import argparse
+
+# GLOBAL CONSTANTS #############################################################
+
+DATABASE_FILE_URL = r"https://raw.githubusercontent.com/drxvmrz/Q-descriptor/refs/heads/main/database/database.txt"
+
--- scripts/update_database.py
+++ scripts/update_database.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# This scrips scans a specified folder 
+# This script scans a specified folder 
 # to update 'database.txt' file
 #
 # INITS ########################################################################
@@ -13,14 +13,46 @@ __email__ = "pddrozhilkin@yandex.ru"
 
 import os
 import sys
+import argparse
 
 # GLOBAL CONSTANTS #############################################################
 
-DATABASE_FILE = os.path.normpath(os.path.join("..", "database", "data.txt"))
+DATABASE_REPO_PATH = os.path.normpath(os.path.join("..", "database", "database.txt"))
 
 # CLASSES ######################################################################
 
+class Settings:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="This script scans a specified folder to update 'database.txt' file")
+        # non-options
+        self.parser.add_argument("good_cifs_dir", type=str, help="Path to the folder contains .cif-files meets the Q-criteria")
+        # options
+        self.parser.add_argument("-db", "--database", type=str, default=f"{os.path.abspath(DATABASE_REPO_PATH)}", help="Path to file 'database.txt'")
 
+        self.good_cifs_dir = ""
+        self.database_path = ""
+
+    def is_valid_args(self):
+        return os.path.exists(self.good_cifs_dir) and os.path.exists(self.database_patha)
+
+    def parse_args(self):
+        args = self.parser.parse_args()
+        
+        self.good_cifs_dir = args.good_cifs_dir
+        self.database_path = args.database
+
+class Database:
+    def __init__(self):
+        pass
+
+    def download_last_ver(self):
+        pass
+
+    def get_new_refcodes(self):
+        pass
+
+    def update(self):
+        pass
 
 # MAIN #########################################################################
 
