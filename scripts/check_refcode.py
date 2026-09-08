71945d4ef7197af70ea256207745fc972481efff
Paul D. <pddrozhilkin@yandex.ru>
2026-09-08 16:48:03 +0300

    new

--- scripts/check_refcode.py
+++ scripts/check_refcode.py
@@ -12,6 +12,8 @@ __email__ = "pddrozhilkin@yandex.ru"
 
 import os
 import sys
+import urllib.request
+import urllib.error
 import argparse
 
 # GLOBAL CONSTANTS #############################################################
@@ -25,7 +27,7 @@ class Settings:
         self.parser = argparse.ArgumentParser(description="Scan 'database.txt' for input refcodes")
         # non-options
         self.parser.add_argument("refcodes", nargs="+", type=str, help="The one or many refcodes to scan")
-        self.refcode_list = []
+        self.refcode_list : list[str] = []
 
     def is_valid_args(self):
         for code in self.refcode_list:
@@ -36,10 +38,44 @@ class Settings:
         args = self.parser.parse_args()
         self.refcode_list = args.refcodes
 
-# FUNCTIONS ####################################################################
+class Checker:
+    def __init__(self):
+        self.database : list[str] = []
+        self.search_status = dict()
 
-def check_ref_in_database(settings : Settings):
-    pass
+    def output_search_stat(self):
+        if len(self.database) == 0: return
+
+        print("Search results! Check on DSC or XRD if \033[32mTrue\033[0m")
+        for key in self.search_status:
+            if self.search_status[key]:
+                print(f"{key}: \033[32mTrue\033[0m")
+            else:
+                print(f"{key}: \033[31mFalse\033[0m")
+
+    def check_refs_in_db(self, ref_to_search_list):
+        if len(self.database) == 0: return
+
+        for ref in ref_to_search_list:
+            self.search_status[ref] = (ref in self.database)
+
+    def download_db(self):
+        try:
+            # Открываем соединение
+            with urllib.request.urlopen(DATABASE_FILE_URL) as response:
+                # Читаем байты и декодируем в UTF-8
+                html = response.read().decode('utf-8')
+                self.database = html.split(",")
+                
+                print(f"Database has been download succesfully!")
+                return html
+                 
+        except urllib.error.HTTPError as e:
+            print(f"Connection Error! {e.code} - {e.reason}")
+        except urllib.error.URLError as e:
+            print(f"Connection Error! {e.reason}")
+        except Exception as e:
+            print(f"Unknown Error! {e}")
 
 # MAIN #########################################################################
 
@@ -51,5 +87,9 @@ def main():
         print("ERROR! Invalid REFCODE")
         exit(1)
 
+    checker = Checker()
+    checker.download_db()
+    checker.check_refs_in_db(sets.refcode_list)
+    checker.output_search_stat()
 
 main()
